3b95f89c474dd81d4c3e6f17ee64f307c72ef859
Kasper Johansen <k@spernj.org>
2012-07-01 22:23:44 +0200

    Added thread-safety. Raises corruption error when calling each more than once. Added lots more documentation.

---
+++ lib/array_enumerator.rb
@@ -0,0 +1,173 @@
+#This class is ment as an enumerator but with a cache that enables it to emulate array-functionality (first, empty and so on). If elements goes out of memory, then the array becomes corrupted and methods like 'first' and 'slice' will no longer work (raise errors).
+class Array_enumerator
+  #Takes an enumerator to work with as argument.
+  def initialize(enum)
+    #The enumerator being used.
+    @enum = enum
+    
+    #Used to calculate length without depending corruption.
+    @length_cache = 0
+    
+    #If the virtual array has become corrupted because of forgotten elements (by calling each and enumerating through elements).
+    @array_corrupted = false
+    
+    #To allow the object to be thread-safe.
+    @mutex = Mutex.new
+  end
+  
+  #Cache the first elements (if not cached already) and returns it.
+  def first
+    check_corrupted
+    cache_ele if !@eles or @eles.empty?
+    return @eles.first
+  end
+  
+  #Returns true if the array is empty.
+  def empty?
+    cache_ele if @eles == nil
+    return @empty
+  end
+  
+  #Returns each element and releases them from cache.
+  def each(&block)
+    if block
+      to_enum.each(&block)
+      return nil
+    else
+      return to_enum
+    end
+  end
+  
+  #Returns a enumerator that can yield the all the lements (both cached and future un-cached ones).
+  def to_enum
+    check_corrupted
+    @array_corrupted = true
+    
+    enum = Enumerator.new do |yielder|
+      if @eles
+        while ele = @eles.shift
+          yielder << ele
+        end
+      end
+      
+      yield_rest do |ele|
+        yielder << ele
+      end
+    end
+  end
+  
+  #Forcing all elements to be cached and crush the memory for big results.
+  def length
+    cache_all
+    return @length_cache
+  end
+  
+  #Giving slice negaive arguments will force it to cache all elements and crush the memory for big results.
+  def slice(*args)
+    check_corrupted
+    
+    if args[0].is_a?(Range) and !args[1]
+      need_eles = args[0].begin + args[0].end
+    elsif args[0] and !args[1]
+      need_eles = args[0]
+    elsif args[0] and args[1] and args[0] > 0 and args[1] > 0
+      need_eles = args[0] + args[1]
+    elsif args[0] < 0 or args[1] < 0
+      cache_all
+    else
+      raise "Dont now what to do with args: '#{args}'."
+    end
+    
+    @eles = [] if !@eles
+    cache_eles = need_eles - @eles.length if need_eles
+    cache_ele(cache_eles) if need_eles and cache_eles > 0
+    return @eles.slice(*args)
+  end
+  
+  #Caches necessary needed elements and then returns the result as on a normal array.
+  def shift(*args)
+    if args[0]
+      amount = args[0]
+    else
+      amount = 1
+    end
+    
+    @eles = [] if !@eles
+    cache_ele(amount - @eles.length) if !@eles or @eles.length < amount
+    res = @eles.shift(*args)
+    
+    #Since we are removing an element, the length should go down with the amount of elements captured.
+    if args[0]
+      @length_cache -= res.length
+    else
+      @length_cache -= 1
+    end
+    
+    return res
+  end
+  
+  #Returns a normal array with all elements. Can also raise corrupted error if elements have been thrown out.
+  def to_a
+    check_corrupted
+    cache_all
+    return @eles
+  end
+  
+  alias to_ary to_a
+  
+  private
+  
+  #Raises error because elements have been forgotten to spare memory.
+  def check_corrupted
+    raise "Too late to call. Corrupted." if @array_corrupted
+  end
+  
+  #Yields the rest of the elements to the given block.
+  def yield_rest
+    @array_corrupted = true
+    
+    begin
+      @mutex.synchronize do
+        while ele = @enum.next
+          @length_cache += 1
+          yield(ele)
+        end
+      end
+    rescue StopIteration
+      #ignore.
+    end
+  end
+  
+  #Caches a given amount of elements.
+  def cache_ele(amount = 1)
+    @eles = [] if !@cache
+    
+    begin
+      @mutex.synchronize do
+        while @eles.length <= amount
+          @eles << @enum.next
+          @length_cache += 1
+          @empty = false
+        end
+      end
+    rescue StopIteration
+      #ignore.
+    end
+  end
+  
+  #Forces the rest of the elements to be cached.
+  def cache_all
+    @eles = [] if !@eles
+    
+    begin
+      @mutex.synchronize do
+        while ele = @enum.next
+          @length_cache += 1
+          @eles << ele
+        end
+      end
+    rescue StopIteration
+      #ignore.
+    end
+  end
+end
--- spec/array_enumerator_spec.rb
+++ spec/array_enumerator_spec.rb
@@ -1,7 +1,113 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "ArrayEnumerator" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "shift" do
+    cont = %w[a b c d e].to_enum
+    ae = Array_enumerator.new(cont)
+    
+    res = ae.shift
+    raise "Expected 'a' but got: '#{res}'." if res != "a"
+    
+    arr = ae.to_a
+    exp = %w[b c d e]
+    raise "Expected array to be '#{exp}' but got: '#{arr}'." if arr != exp
+  end
+  
+  it "each" do
+    arr = %w[a b c]
+    cont = arr.to_enum
+    ae = Array_enumerator.new(cont)
+    
+    count = 0
+    ae.each do |ele|
+      raise "Expected #{arr[count]} but got: #{ele}" if ele != arr[count]
+      count += 1
+    end
+    
+    raise "Expected 3 elements to yield through but got: #{count}" if count != 3
+    
+    arr = %w[a b c]
+    cont = arr.to_enum
+    ae = Array_enumerator.new(cont)
+    
+    res = ae.empty?
+    raise "Expected empty to be false but got: '#{res}'." if res != false
+    
+    count = 0
+    ae.each do |ele|
+      raise "Expected #{arr[count]} but got: #{ele}" if ele != arr[count]
+      count += 1
+    end
+    
+    raise "Expected 3 elements to yield through but got: #{count}" if count != 3
+    
+    begin
+      ae.to_a
+      raise "Should have raised exception but didnt."
+    rescue
+      #ignore.
+    end
+    
+    begin
+      ae.first
+      raise "Should have raised exception but didnt."
+    rescue
+      #ignore.
+    end
+    
+    begin
+      ae.each do |ele|
+        raise "Should get here?"
+      end
+      
+      raise "Should get here?"
+    rescue
+      #ignore.
+    end
+  end
+  
+  it "empty" do
+    cont = %w[a b c].to_enum
+    ae = Array_enumerator.new(cont)
+    
+    res = ae.empty?
+    raise "Expected empty to be false but got: '#{res}'." if res != false
+    
+    first = ae.first
+    raise "Expected first to be 'a' but it wasnt: '#{first}'." if first != "a"
+  end
+  
+  it "length" do
+    cont = %w[a b c d e].to_enum
+    ae = Array_enumerator.new(cont)
+    
+    res = ae.length
+    raise "Expected length to be 5 but it wasnt: '#{res}'." if res != 5
+    
+    ae.shift
+    
+    res = ae.length
+    raise "Expected length to be 4 but it wasnt: '#{res}'." if res != 4
+  end
+  
+  it "slice" do
+    arr = %w[a b c d e f g h i j k l m n]
+    cont = arr.to_enum
+    ae = Array_enumerator.new(cont)
+    
+    runs = [
+      [6],
+      [1, 2],
+      [1..3],
+      [-2, 2],
+      [2, -2]
+    ]
+    
+    runs.each do |args|
+      res1 = arr.slice(*args)
+      res2 = ae.slice(*args)
+      
+      raise "Expected res to be: #{res1} but got #{res2} for args '#{args}'." if res1 != res2
+    end
   end
 end
