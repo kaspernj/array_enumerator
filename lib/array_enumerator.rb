502d143f364473ff81fbdc63f4000be5aee70da3
kaspernj <k@spernj.org>
2014-12-19 15:55:48 +0100

    Fixed space after comment and EOF

--- lib/array_enumerator.rb
+++ lib/array_enumerator.rb
@@ -1,9 +1,9 @@
-#This class is ment as an enumerator but with a cache that enables it to emulate array-functionality (first, empty and so on). If elements goes out of memory, then the array becomes corrupted and methods like 'first' and 'slice' will no longer work (raise errors).
+# This class is ment as an enumerator but with a cache that enables it to emulate array-functionality (first, empty and so on). If elements goes out of memory, then the array becomes corrupted and methods like 'first' and 'slice' will no longer work (raise errors).
 class ArrayEnumerator
   class ArrayCorruptedError < RuntimeError; end
   class CannotCallBeforeEnd < RuntimeError; end
 
-  #Takes an enumerator to work with as argument.
+  # Takes an enumerator to work with as argument.
   def initialize(enum = nil, &blk)
     if enum
       #The enumerator being used.
@@ -16,24 +16,24 @@ class ArrayEnumerator
       raise "No enum or block was given."
     end
 
-    #Used to calculate length without depending corruption.
+    # Used to calculate length without depending corruption.
     @length_cache = 0
 
-    #If the virtual array has become corrupted because of forgotten elements (by calling each and enumerating through elements).
+    # If the virtual array has become corrupted because of forgotten elements (by calling each and enumerating through elements).
     @array_corrupted = false
 
-    #To allow the object to be thread-safe.
+    # To allow the object to be thread-safe.
     @mutex = Mutex.new
   end
 
-  #Cache the first elements (if not cached already) and returns it.
+  # Cache the first elements (if not cached already) and returns it.
   def first
     check_corrupted
     cache_ele if !@eles || @eles.empty?
     return @eles.first
   end
 
-  #Returns true if the array is empty.
+  # Returns true if the array is empty.
   def empty?
     if @empty == nil
       cache_ele if !@eles
@@ -48,7 +48,7 @@ class ArrayEnumerator
     return @empty
   end
 
-  #Returns each element and releases them from cache.
+  # Returns each element and releases them from cache.
   def each(&block)
     if block
       to_enum.each(&block)
@@ -58,7 +58,7 @@ class ArrayEnumerator
     end
   end
 
-  #This method should only be used with 'each_index'.
+  # This method should only be used with 'each_index'.
   def [](key)
     if @each_index && @each_index.key?(key)
       ret = @each_index[key]
@@ -69,7 +69,7 @@ class ArrayEnumerator
     raise "This only works when also using 'each_index'. Invalid key: '#{key}'."
   end
 
-  #Yields each count-key and caches element for returning it by using the []-method.
+  # Yields each count-key and caches element for returning it by using the []-method.
   def each_index(&block)
     enum = Enumerator.new do |yielder|
       begin
@@ -77,15 +77,15 @@ class ArrayEnumerator
 
         count = 0
         self.each do |ele|
-          #Remove previous element to not take up memory.
+          # Remove previous element to not take up memory.
           count_before = count - 1
           @each_index.delete(count_before) if @each_index.key?(count_before)
 
-          #Add current element to cache.
+          # Add current element to cache.
           @each_index[count] = ele
           yield(count)
 
-          #Increase count for next run.
+          # Increase count for next run.
           count += 1
         end
       ensure
@@ -101,7 +101,7 @@ class ArrayEnumerator
     end
   end
 
-  #Returns a enumerator that can yield the all the lements (both cached and future un-cached ones).
+  # Returns a enumerator that can yield the all the lements (both cached and future un-cached ones).
   def to_enum
     check_corrupted
     @array_corrupted = true
@@ -119,13 +119,13 @@ class ArrayEnumerator
     end
   end
 
-  #Returns the counted length. Can only be called after the end of the enumerator has been reached.
+  # Returns the counted length. Can only be called after the end of the enumerator has been reached.
   def length
     raise CannotCallBeforeEnd, "Cant call length before the end has been reached." unless @end
     return @length_cache
   end
 
-  #Giving slice negaive arguments will force it to cache all elements and crush the memory for big results.
+  # Giving slice negaive arguments will force it to cache all elements and crush the memory for big results.
   def slice(*args)
     check_corrupted
 
@@ -147,7 +147,7 @@ class ArrayEnumerator
     return @eles.slice(*args)
   end
 
-  #Caches necessary needed elements and then returns the result as on a normal array.
+  # Caches necessary needed elements and then returns the result as on a normal array.
   def shift(*args)
     check_corrupted
 
@@ -161,7 +161,7 @@ class ArrayEnumerator
     cache_ele(amount - @eles.length) if !@eles || @eles.length < amount
     res = @eles.shift(*args)
 
-    #Since we are removing an element, the length should go down with the amount of elements captured.
+    # Since we are removing an element, the length should go down with the amount of elements captured.
     if args[0]
       @length_cache -= res.length
     else
@@ -171,7 +171,7 @@ class ArrayEnumerator
     return res
   end
 
-  #Returns a normal array with all elements. Can also raise corrupted error if elements have been thrown out.
+  # Returns a normal array with all elements. Can also raise corrupted error if elements have been thrown out.
   def to_a
     check_corrupted
     cache_all
@@ -182,12 +182,12 @@ class ArrayEnumerator
 
 private
 
-  #Raises error because elements have been forgotten to spare memory.
+  # Raises error because elements have been forgotten to spare memory.
   def check_corrupted
     raise ArrayCorruptedError, "Too late to call. Corrupted." if @array_corrupted
   end
 
-  #Yields the rest of the elements to the given block.
+  # Yields the rest of the elements to the given block.
   def yield_rest
     @array_corrupted = true
 
@@ -203,7 +203,7 @@ private
     end
   end
 
-  #Caches a given amount of elements.
+  # Caches a given amount of elements.
   def cache_ele(amount = 1)
     @eles = [] if !@cache
 
@@ -219,7 +219,7 @@ private
     end
   end
 
-  #Forces the rest of the elements to be cached.
+  # Forces the rest of the elements to be cached.
   def cache_all
     @eles = [] if !@eles
 
